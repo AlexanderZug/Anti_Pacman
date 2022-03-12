02d0587b81d9f1371b8721309ddd0540a1f0f69a
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-03-12 14:36:58 +0300

    add description to game_start_over_window.py

--- game_code/game_start_over_window.py
+++ game_code/game_start_over_window.py
@@ -6,6 +6,7 @@ from game_settings import GameSettings
 
 
 class GameStartOverWindows:
+    """Класс, создающий стартвое окно и окно Game Over; создает экземпляр класса GameSettings"""
     def __init__(self, window_surface):
         self.window_surface = window_surface
         self.speed = 3
@@ -16,6 +17,8 @@ class GameStartOverWindows:
         self.over_sound = pygame.mixer.Sound('games_music/game_over.wav')
 
     def titel_lbl(self):
+        """Метод, создает и выводит на игровое поле название игры и
+        информацию о необходимости нажатия клавиши для начала"""
         titel_text = GameSettings().start_font.render("AntiPacman", True, GameSettings().start_color)
         titel_rect = titel_text.get_rect()
         titel_rect.center = (300, 280)
@@ -26,6 +29,7 @@ class GameStartOverWindows:
         self.window_surface.blit(press_key_text, press_key_rect)
 
     def unit_move_start_window(self):
+        """Метод, выводящий на стартовый экран анимацию"""
         ghost_img = pygame.image.load('games_photos/ghost.png')
         ghost_img = pygame.transform.scale(ghost_img, (40, 40))
         pacman_img = pygame.image.load('games_photos/enemy.png')
@@ -52,6 +56,7 @@ class GameStartOverWindows:
         pygame.display.update()
 
     def game_over_titel(self, score, lvl):
+        """Метод, выводящий в случае поражения Game Over окно с печальной музыкой"""
         game_over_titel_text = GameSettings().start_font.render("Game Over", True, GameSettings().start_color)
         game_over_titel_rect = game_over_titel_text.get_rect()
         game_over_titel_rect.center = (300, 280)
@@ -72,12 +77,15 @@ class GameStartOverWindows:
         self.over_sound.play()
 
     def game_over_photos(self):
+        """Метод, выводит на Game Over окно фото с расстроенными призраками"""
         end_ghosts_img = pygame.image.load('games_photos/end_photo.png')
         end_ghosts_img = pygame.transform.scale(end_ghosts_img, (150, 150))
         end_ghosts_rect = end_ghosts_img.get_rect(center=(400, 150))
         self.window_surface.blit(end_ghosts_img, end_ghosts_rect)
 
     def wait_for_gamer_query_start(self):
+        """Метод, создающий игровой цикл и ждущий от игрока нажатия клавиши для старта игры;
+        отрисовывает стартовые надписи и анимацию"""
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -92,12 +100,14 @@ class GameStartOverWindows:
 
     @staticmethod
     def start_music():
+        """Статичный метод для запуска стартовой музыки"""
         pygame.mixer.music.load('games_music/start_song.mp3')
         pygame.mixer.music.play(-1, 0.0)
         pygame.mixer.music.set_volume(0.09)
 
     @staticmethod
     def stop_start_music():
+        """Статичный метод для остановки стартовой музыки"""
         pygame.mixer.music.stop()
 
 
